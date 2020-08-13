bfb115c0edaa6ed478070a419975bd9f9c986b53
Justin Krometis <jkrometis@vt.edu>
2020-08-13 11:02:51 -0400

    initial commit for tc hdf5

---
+++ hdf5/hdf5_tinkercliffs_rome.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#
+#SBATCH -t 00:05:00
+#SBATCH -N1 --ntasks-per-node=1
+#SBATCH -p dev_q
+#
+
+#
+module reset
+module load HDF5
+#
+echo "HDF5_TINKERCLIFFS ROME: Normal beginning of execution."
+#
+h5cc -c hdf5_test.c
+if [ $? -ne 0 ]; then
+  echo "HDF5_TINKERCLIFFS ROME: Compile error."
+  exit 1
+fi
+#
+h5cc -o hdf5_test hdf5_test.o
+if [ $? -ne 0 ]; then
+  echo "HDF5_TINKERCLIFFS ROME: Load error."
+  exit 1
+fi
+rm hdf5_test.o
+#
+./hdf5_test > hdf5_tinkercliffs_rome.txt
+if [ $? -ne 0 ]; then
+  echo "HDF5_TINKERCLIFFS ROME: Run error."
+  exit 1
+fi
+rm hdf5_test
+#
+echo "HDF5_TINKERCLIFFS ROME: Normal end of execution."
+exit 0
