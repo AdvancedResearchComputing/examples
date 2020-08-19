2be6f5081635166ac1a913d011b1a1db909e0d9f
Justin Krometis <jkrometis@vt.edu>
2020-08-19 15:21:32 -0400

    tweaks to vasp submission script

--- vasp/vasp_tinkercliffs_rome.sh
+++ vasp/vasp_tinkercliffs_rome.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #
-#SBATCH -t 5:00
+#SBATCH -t 30:00
 #SBATCH -n 4
 #SBATCH -p dev_q
 #
@@ -16,8 +16,7 @@ echo "VASP_TINKERCLIFFS ROME: Normal beginning of execution."
 #
 #  we use the following command, which sets the stacksize to "unlimited":
 #
-#mpirun -np 4 /bin/bash -c "ulimit -s unlimited; vasp_std"
-srun -n 4 /bin/bash -c "ulimit -s unlimited; vasp_std"
+mpirun -np 4 /bin/bash -c "ulimit -s unlimited; vasp_std"
 if [ $? -ne 0 ]; then
   echo "VASP_TINKERCLIFFS ROME: Run error!"
   exit 1
