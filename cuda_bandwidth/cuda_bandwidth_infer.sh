f163eb8a6552f1af62d657896ae3b91c3528c407
Justin Krometis <jkrometis@vt.edu>
2021-01-19 13:38:29 -0500

    simplify infer cuda_bandwidth script

--- cuda_bandwidth/cuda_bandwidth_infer.sh
+++ cuda_bandwidth/cuda_bandwidth_infer.sh
@@ -5,26 +5,11 @@
 #SBATCH -p t4_dev_q
 #
 
-#
+#Load CUDA module
 module reset
 module load CUDA/11.1.1-GCC-10.2.0
 #
-echo "CUDA BANDWIDTH_INFER: Normal beginning of execution."
-#
-#  Discard the previous output file, if any.
-#
-if [ -e cuda_bandwidth_infer.txt ]; then
-  rm cuda_bandwidth_infer.txt
-fi
 
 #Run CUDA's bandwidth test
-$EBROOTCUDA/extras/demo_suite/bandwidthTest --memory=pinned --mode=quick --dtoh | tee cuda_bandwidth_infer.txt
-if [ $? -ne 0 ]; then
-  echo "CUDA BANDWIDTH_INFER: Run error!"
-  exit 1
-fi
-#
-#  Terminate.
-#
-echo "CUDA BANDWIDTH_INFER: Normal end of execution."
-exit 0
+$EBROOTCUDA/extras/demo_suite/bandwidthTest --memory=pinned --mode=quick --dtoh
+
