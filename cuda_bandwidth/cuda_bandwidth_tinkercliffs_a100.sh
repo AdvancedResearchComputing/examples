a671823c5959e8aa8ce1b62309f74f734f453e2b
Justin Krometis <jkrometis@vt.edu>
2021-06-11 09:41:18 -0400

    cuda bandwidth tc a100: reduce to 1 gpu, load cuda toolkit (hopefully temporary)

--- cuda_bandwidth/cuda_bandwidth_tinkercliffs_a100.sh
+++ cuda_bandwidth/cuda_bandwidth_tinkercliffs_a100.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 #
 #SBATCH -t 00:05:00
-#SBATCH -N1 --ntasks-per-node=128 --gres=gpu:8
+#SBATCH -N1 --ntasks-per-node=1 --gres=gpu:1
 #SBATCH -p a100_dev_q
 #
 
 #Load CUDA module
 module reset
+module load cuda11.2/toolkit
 module load CUDA/11.1.1-GCC-10.2.0
 #
 
