903a2b595c6c955e0438ac847841d2a855001ebe
Justin Krometis <jkrometis@vt.edu>
2021-09-23 16:43:43 -0400

    wrf: switch from em_b_wave to em_tropical_cyclone example since the former wouldn't work, switch to wrf 4.2.2, confirmed working

--- wrf/wrf_tinkercliffs_rome.sh
+++ wrf/wrf_tinkercliffs_rome.sh
@@ -1,21 +1,29 @@
 #! /bin/bash
+#Example script for running WRF on ARC systems
+# - Input files input_sounding and namelist.input copied from
+#   test/em_tropical_cyclone in the WRF install directory
+# - namelist.input edited to simulate 1 day rather than 6 to 
+#   reduce runtime
 #
-#SBATCH -t 00:10:00
+#SBATCH -t 00:30:00
 #SBATCH -N1 --ntasks-per-node=4
 #SBATCH -p dev_q
 #
 
 #load modules
 module reset
-module load WRF
+module load WRF/4.2.2-foss-2020b-dmpar
 
-#link
-echo "Setting up b_wave case by linking data files into this directory"
-echo "linking to RRTM_DATA in WRF's run directory"
-ln -sf "$EBROOTWRF/WRF-4.1.3/run/RRTM_DATA" .
+#symlink to required input data (mimic run_me_first.csh from example)
+ln -sf "$EBROOTWRF/WRF-4.2.2/run/LANDUSE.TBL" .
+ln -sf "$EBROOTWRF/WRF-4.2.2/run/RRTM_DATA" .
 
 #run ideal.exe to generate input files
 ideal.exe
+
+#move the outputs from ideal.exe so they're not overwritten by wrf
+mv rsl.error.0000 ideal_rsl.error.0000
+mv rsl.out.0000 ideal_rsl.out.0000
 
 #run WRF
 mpirun -np $SLURM_NTASKS wrf.exe
