fd825eb245a150962867ebb9cf785ce50ccabb65
Justin Krometis <jkrometis@vt.edu>
2021-10-12 13:25:04 -0400

    r on ca: clean up some comments

--- r/mcpi_parallel_cascades.sh
+++ r/mcpi_parallel_cascades.sh
@@ -10,10 +10,6 @@
 module purge
 module load intel/18.2 openmpi/4.0.1 R/3.6.1 R-parallel/3.6.1
 
-# OpenMPI environment variables to fix hangs
-#export OMPI_MCA_btl_openib_allow_ib=1 #allow infiniband
-#export OMPI_MCA_rmaps_base_inherit=1  #child processes inherit environment
-
 # Run R
 #Run as a script; this works but the .r file must be changed so the number
 #of child processes is 1 less than the number of cores allocated
@@ -21,7 +17,6 @@ module load intel/18.2 openmpi/4.0.1 R/3.6.1 R-parallel/3.6.1
 
 #Start with MPI
 # --oversubscribe so we can start as many child processes as cores
-#mpirun -np 1 --bind-to none --oversubscribe Rscript mcpi_parallel.r 
 mpirun -np 1 --oversubscribe Rscript mcpi_parallel.r 
 
 exit;
