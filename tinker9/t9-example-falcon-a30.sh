31ed6cfb02573a2431ed6d78a2f6d3c9c41dddc5
Samira Mali <smali@falcon1.cm.falcon>
2025-07-08 16:48:24 -0400

    Update A30 job script for Tinker9 example

--- tinker9/t9-example-falcon-a30.sh
+++ tinker9/t9-example-falcon-a30.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --account=<your account>
+#SBATCH --account=personal
 #SBATCH --partition=a30_normal_q
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
@@ -7,36 +7,33 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --time=0-1:00:00
 
-# Each node type has different modules avilable. Resetting makes the appropriate stack available
 module reset
-module load falcon-sapphirerapids/tinker9/1.4.0-nvhpc-23.7
+module load Tinker9/1.4.0-NVHPC-25.1
 
-# We will use it, so make sure the USERNAME variable is set correctly
-if [ "$USER" != `id -un` ]; then
-  USER=`id -un`
-fi
-
-# Make a directory on /scratch for the test if it doesn't already exist
-# /scratch is preferred over /home or /projects for staging and running jobs
 WORKDIR="/scratch/${USER}/t9-example"
-if [ ! -d "$WORKDIR" ]; then
-  mkdir -p $WORKDIR
+mkdir -p "$WORKDIR"
+cd "$WORKDIR"
+
+# Confirm Tinker9 root
+echo "EBROOTTINKER9=$EBROOTTINKER9"
+ls -l $EBROOTTINKER9
+
+# Try to copy example and params
+if [ -d "$EBROOTTINKER9/example" ]; then
+  cp -vr $EBROOTTINKER9/{example,params} .
+elif [ -d "$EBROOTTINKER9/tinker9/example" ]; then
+  cp -vr $EBROOTTINKER9/tinker9/{example,params} .
+else
+  echo "ERROR: Cannot locate example or params directories."
+  exit 1
 fi
 
-# Tinker9 installation has example files. Copying them to working directory
-cp -vr $EBROOTTINKER9/tinker9/{example,params} .
-cd example
+cd example || { echo "example dir not found"; exit 1; }
 
-# Diagnostic output
-echo "working in `pwd`"
-ls -l
-
-# Start background process to log GPU utilization to a file
-# This example should run at about 97% GPU utilization and <1GB of device memory while running
-/apps/useful_scripts/bin/gpumon > $SLURM_SUBMIT_DIR/job-${SLURM_JOB_ID}-gpu.log &
-
+# GPU monitor and run
+/apps/common/useful_scripts/gpumon > $SLURM_SUBMIT_DIR/job-${SLURM_JOB_ID}-gpu.log &
 tinker9 info
-# Run the example. Expected runtime for this example is 1 minute on a A30 GPU
 echo "-------- Starting tinker9: `date` -------"
 tinker9 dynamic dhfr2.xyz 5000 2 1 2 298
 echo "------- tinker9 has exited: `date` --------"
+
