6fea42bf02474f751021cfbe62946c54adc41926
Samira Mali <smali@owl1.cm.owl>
2025-07-18 14:00:25 -0400

    Change number of GPUs used in job script

--- Amber/run_tinkercliffs_gpu.sh
+++ Amber/run_tinkercliffs_gpu.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name=amber_md_gpu
 #SBATCH --account=personal
 #SBATCH --partition=a100_normal_q
-#SBATCH --gres=gpu:3
+#SBATCH --gres=gpu:2
 #SBATCH --nodes=1
-#SBATCH --ntasks=3
+#SBATCH --ntasks=2
 #SBATCH --cpus-per-task=4
 #SBATCH --time=00:05:00
 
 module reset
 module load Amber/24.0-foss-2023b-AmberTools-24.0-CUDA-12.4.0
 
-mpirun -np 3 pmemd.cuda.MPI -O -i ala.in -o ala.out -p ala.top -c ala.rst7 -r ala_prod.rst7 -x ala.nc
+mpirun -np 2 pmemd.cuda.MPI -O -i ala.in -o ala.out -p ala.top -c ala.rst7 -r ala_prod.rst7 -x ala.nc
